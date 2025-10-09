3603fb6a66db3a4e45799099e33b35420b179456
soyeonkim8888 <project36bys@gmail.com>
2025-10-10 01:18:08 +0900

    Feature/6 feed review (#40)
    * [#6]feat(feed):add Reivew feature & corresponding schema-postres(#18)
    
    * [#6] feat(feed) : add likes feature (#18)
    
    * [#14]feat(moderation):add profanity-wordFilter with hardcoding(#22)
    
    * [#14]feat(moderation):add report review(#23)
    
    * [#14]refactor(moderation):refactor reviewService.java to apply filtering(#26)
    
    * [#14]refactor(moderation):refactor reviewService.java to apply filtering(#26)
    
    * [#6]refactor(feed):refactor based on feedback(#31)
    
    * [#6]refactory(feed):responseDTO return value edit(#31)
    
    * [#14]refactor(moderation):report count only stay(#34)
    
    * [#29]feat(stat):add statistics feature(#35)
    
    * [#6]refactor(feed):simplify code logic and flow(#31)
    
    * [#14]refactor(moderation):simply report feature(#34)
    
    * [#14]refactor(moderation):refactor feed/report/stats(#34)
    
    * [#14]refactor(moderation):refactor feed/report/stats(#34)
    
    * [#6]refactor(feed):refactor visit/place package(#31)
    
    * [#6]refactor: swagger codde added/ conflict fixed(#31)
    
    * [#6]refactor: add user_name to schema-postgres.sql(#31)
    
    * [#6]refactor:application-dev.yml to add test seed sql(#31)
    
    ---------
    
    Co-authored-by: 김소연 <kimsoyeon@gimsoyeon-ui-MacBookPro.local>

--- src/main/java/com/matjom/matjom/common/exception/message/ErrorCode.java
+++ src/main/java/com/matjom/matjom/common/exception/message/ErrorCode.java
@@ -47,6 +47,7 @@ public enum ErrorCode {
 
     // ====== Feed ======
     REVIEW_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "리뷰 작성이 허용되지 않습니다."),
+    REVIEW_NOT_FOUND(HttpStatus.BAD_REQUEST, "리뷰를 찾을 수 없습니다"),
     REVIEW_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 리뷰를 작성했습니다."),
     REVIEW_BAD_LANGUAGE(HttpStatus.BAD_REQUEST, "부적절한 표현이 감지되었습니다."),
     LIKE_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "좋아요를 누를 수 없습니다."),
